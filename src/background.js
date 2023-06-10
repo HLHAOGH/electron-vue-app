f3937ad3dd645e583e14db0b99567c47945e8d7e
lihao <2579068228@qq.com>
2023-06-10 10:11:16 +0800

    优化环境配置

--- src/background.js
+++ src/background.js
@@ -1,26 +1,30 @@
 'use strict'
 
-import { app, protocol, BrowserWindow } from 'electron'
+import { app, protocol, BrowserWindow, Tray, Menu } from 'electron'
 import { createProtocol } from 'vue-cli-plugin-electron-builder/lib'
 import installExtension, { VUEJS_DEVTOOLS } from 'electron-devtools-installer'
-const isDevelopment = process.env.NODE_ENV !== 'production'
+const path = require('path');
+
+const isDevelopment = process.env.NODE_ENV !== 'production';
 
 // Scheme must be registered before the app is ready
 protocol.registerSchemesAsPrivileged([
-  { scheme: 'app', privileges: { secure: true, standard: true } }
+  { scheme: 'app', privileges: { secure: true, standard: true, stream: true } }
 ])
 
+let win;
 async function createWindow() {
   // 创建并控制浏览器窗口
-  const win = new BrowserWindow({
+  win = new BrowserWindow({
     width: 800,
     height: 600,
+    frame: true, // 创建无边框窗口（不能拖动窗口）
     // 网页功能设置
     webPreferences: {
       nativeWindowOpen: true, // 原生 window.open () 允许同步打开窗口
       enableRemoteModule: true, // 将 JavaScript 对象从主进程桥接到渲染器进程
       devTools: true,
-      webSecurity: true, // 是否启用同源策略
+      webSecurity: false, // 是否允许跨域
       allowRunningInsecureContent: false, // 允许一个 https 页面运行来自http url的JavaScript, CSS 或 plugins
       images: true, // 允许加载图片
       webgl: true, //  启用 WebGL 支持
@@ -29,7 +33,7 @@ async function createWindow() {
       spellcheck: true, // 是否启用内置拼写检查器
       // Use pluginOptions.nodeIntegration, leave this alone
       // See nklayman.github.io/vue-cli-plugin-electron-builder/guide/security.html#node-integration for more info
-      nodeIntegration: process.env.ELECTRON_NODE_INTEGRATION, // 是否启用Node integration节点继承
+      nodeIntegration: process.env.ELECTRON_NODE_INTEGRATION, // 是否启用Node环境继承
       contextIsolation: !process.env.ELECTRON_NODE_INTEGRATION // 是否在独立 JavaScript 环境中运行 Electron API和指定的preload 脚本. 默认为 true
     }
   })
@@ -44,6 +48,51 @@ async function createWindow() {
     win.loadURL('app://./index.html')
   }
 }
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', async () => {
+  // 开发环境安装vue-devtools
+  // if (isDevelopment && !process.env.IS_TEST) {
+  //   try {
+  //     await installExtension(VUEJS_DEVTOOLS)
+  //   } catch (e) {
+  //     console.error('Vue Devtools failed to install:', e.toString())
+  //   }
+  // }
+  createWindow();
+  // 隐藏菜单栏
+  // Menu.setApplicationMenu(null);
+  // hide menu for Mac 
+  if (process.platform !== 'darwin') {
+    app.dock.hide();
+  }
+
+  const iconPath = path.join(__dirname, '../build/icons/16x16.png');
+  let tray = new Tray(iconPath) ;     // 实例化一个tray对象，构造函数的唯一参数是需要在托盘中显示的图标url  
+  
+  tray.setToolTip('JSAS');  // 鼠标移到托盘中应用程序的图标上时，显示的文本
+  // 点击图标的响应事件，这里是切换主窗口的显示和隐藏
+  tray.on('click', () => {
+    if (win.isVisible()) {
+      win.hide()
+    } else {
+      win.show()
+    }
+  })
+  // 右键点击图标时，出现的菜单，通过Menu.buildFromTemplate定制，这里只包含退出程序的选项
+  tray.on('right-click', () => {
+    const menuConfig = Menu.buildFromTemplate([
+      {
+        label: 'Quit',
+        click: () => app.quit()
+      }
+    ]);
+    tray.popUpContextMenu(menuConfig)
+  })
+
+})
 
 // Quit when all windows are closed.
 app.on('window-all-closed', () => {
@@ -60,28 +109,6 @@ app.on('activate', () => {
   if (BrowserWindow.getAllWindows().length === 0) createWindow()
 })
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.on('ready', async () => {
-  if (isDevelopment && !process.env.IS_TEST) {
-    // Install Vue Devtools
-    try {
-      await installExtension(VUEJS_DEVTOOLS)
-    } catch (e) {
-      console.error('Vue Devtools failed to install:', e.toString())
-    }
-  }
-  createWindow();
-  // 隐藏菜单栏
-  const { Menu } = require('electron');
-  Menu.setApplicationMenu(null);
-  // hide menu for Mac 
-  if (process.platform !== 'darwin') {
-    app.dock.hide();
-  }
-})
-
 // Exit cleanly on request from parent process in development mode.
 if (isDevelopment) {
   if (process.platform === 'win32') {
@@ -96,3 +123,9 @@ if (isDevelopment) {
     })
   }
 }
+
+// 解决证书错误-https
+app.on('certificate-error', function(event, webContents, url, error, certificate, callback) {
+  event.preventDefault();
+  callback(true);
+});
--- vue.config.js
+++ vue.config.js
@@ -9,7 +9,7 @@ module.exports = {
     // electron配置
 		electronBuilder: {
       // 是否删除electron运行产生的垃圾（警告或错误提示信息）
-      removeElectronJunk: false
+      removeElectronJunk: false,
 			builderOptions: {
         productName: "小蜜蜂",  // 应用的名称
         copyright: "Copyright © 2023 HlHao",
@@ -17,7 +17,16 @@ module.exports = {
           buildResources: "build",   // 指定打包需要的静态资源，默认是build
           output: "dist_electron",  // 打包生成的目录，默认是dist_electron
         },
+        asar: true, // 是否压缩（加密）打包资源文件
+        // 将额外内容打包到resources目录(app.asar是访问资源文件，所有打包文件存在asar文件里)
+        // asar是一个文件归档格式，可以把多个文件归档成一个文件，无需压缩，且具有随机访问能力
+        extraResources: [
+          { from: './static', to: 'static'}
+        ],
+        // 应用程序根目录
+        extraFiles: [],
 				nsis: {
+          displayLanguageSelector: false, // 是否显示多语言安装配置选项
 				  allowToChangeInstallationDirectory: true, // 允许修改安装目录
 				  oneClick: false, // 是否一键安装
           language: "2052", // 安装语言，2052对应中文
@@ -39,7 +48,10 @@ module.exports = {
 				win: {
 					icon: './build/icons/icon.ico', // 打包windows版本的logo，大小必须是256*256的
           target: [
-            'nsis',
+            { 
+              target: 'nsis', // 使用nsis制作安装横向，打包文件后缀为exe
+              arch: ['x64', 'ia32'], // 64位或32位
+            }
           ]
 				},
         mac: {
